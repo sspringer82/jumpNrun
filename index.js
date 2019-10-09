d2b5c5d446c9f237c194e5cf5e59a290b34b23e2
Dominik Ehrenberg <doe@crosscan.com>
2019-10-09 09:53:32 +0200

    Aufgabe 1 - Hintergrund zeichnen+#

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 document.addEventListener('DOMContentLoaded', () => {
-  console.log('Oh Hi Mark');
+  const canvas = document.getElementById('sheep-and-run');
+  const context = canvas.getContext('2d');
+
+  const background = new Background(context);
+  const promise = background.init('assets/background2.png');
+
+  Promise.all([promise]).then(() => {
+    background.render(canvas.width, canvas.height);
+  }) 
 });
