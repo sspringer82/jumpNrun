1bed64cd3c948441fdb6570cbd165c3630ade4a3
Dominik Ehrenberg <doe@crosscan.com>
2019-10-09 11:43:30 +0200

    game loop

--- index.js
+++ index.js
@@ -27,10 +27,12 @@ document.addEventListener('DOMContentLoaded', () => {
   const playerPromise = player.init();
 
   Promise.all([backbroundPromise, ...platformPromises, playerPromise]).then(() => {
-    background.render(canvas.width, canvas.height);
-    platforms.forEach(platform => platform.render());
+
+    const loop = new Loop(context, player, background, platforms);
+
+    
 
     player.y = 202;
-    player.render();
+    requestAnimationFrame(loop.step.bind(loop));
   }) 
 });
